0eac7a4261b793fed5493674a09e5bd4fbe074f8
Лесиков Сергей Игоревич <lesikov@office.crocusgroup.ru>
2024-04-19 19:19:32 +0300

    update

---
+++ ManagerUsersGroups.WpfUI/Converters/EnumFlagsConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ManagerUsersGroups.WpfUI.Converters
+{
+    public class EnumFlagsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
